58e07091f06d603bdbf00206a161b8585f03b098
chippy-1914 <50676688+chippy-1914@users.noreply.github.com>
2020-10-10 22:36:43 +0530

    Add files via upload

---
+++ Assignment Day 2/src/com/assignment/Employee.java
@@ -0,0 +1,14 @@
+package com.assignment;
+
+public class Employee {
+
+        public int age;
+        public String name;
+        public String city;
+
+        public void display() {
+            System.out.println("The name is "+name);
+            System.out.println("The age is "+age);
+            System.out.println("The city is "+city);
+        }
+}
---
+++ Assignment Day 2/src/com/assignment/Main.java
@@ -0,0 +1,19 @@
+package com.assignment;
+public class Main {
+
+    public static void main(String[] args) {
+      Employee E1=new Employee();
+      E1.name="Anne Shirley Cuthbert";
+      E1.age=18;
+      E1.city="Green Gables";
+      Employee E2=new Employee();
+      E2.name="Gilbert";
+      E2.age=19;
+      E2.city="Avonlea";
+      E1.display();
+      E2.display();
+
+
+
+    }
+}
